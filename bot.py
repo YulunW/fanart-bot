2bc273c7510f1715ef0073d63456845f95f3b1b2
Yulun Wang <yulunwan@usc.edu>
2022-09-02 00:52:07 -0700

    Change port

--- bot.py
+++ bot.py
@@ -5,4 +5,4 @@ import config
 if __name__ == '__main__':
     nonebot.init(config)
     nonebot.load_builtin_plugins()
-    nonebot.run(host='127.0.0.1', port=8080)
+    nonebot.run(host='127.0.0.1', port=12345)
