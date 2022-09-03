326f9a2d6f94d61f3687ce00f817dc87525ec6ad
Yulun Wang <yulunwan@usc.edu>
2022-09-02 21:54:28 -0700

    Create random number plugin

--- fanart/plugins/rand_num.py
+++ fanart/plugins/rand_num.py
@@ -17,10 +17,15 @@ async def rand_num(session: CommandSession) -> None:
     elif (match_obj := re.fullmatch(r"(\d+)", args)) is not None:
         end = int(match_obj.group(1))
     elif args != "":
-        await session.send(
-            """参数格式不正确。目前支持的参数格式为：\n"""
-            """一个数字，决定随机数的上限。如输入56会返回一个1~56之间的随机数。\n"""
-            """数字-数字的形式，如2-56。两个数分别为随机数的上下限。""")
+        choices = args.split(" ")
+        start = 0
+        end = len(choices) - 1
+        await session.send(f"随机 [{args}] 结果为：{choices[random.randint(start, end)]}")
         return
+
+    if start > end:
+        await session.send(f"随机数上限{end}小于随机数下限{start}。请输入正确的范围后重试")
+        return
+
     result = random.randint(start, end)
-    await session.send(f"[{start}-{end}]结果为：{result}")
+    await session.send(f"随机 [{start}-{end}] 的结果为：{result}")
