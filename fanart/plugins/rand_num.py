5661b904fc1bf73fafe6aff215419aa1b559405d
Yulun Wang <yulunwan@usc.edu>
2022-09-02 21:46:51 -0700

    Create random number plugin

--- fanart/plugins/rand_num.py
+++ fanart/plugins/rand_num.py
@@ -5,16 +5,16 @@ import re
 from typing import Optional
 
 
-@on_command('random', only_to_me=False, aliases=('随机',))
+@on_command('random', only_to_me=False, aliases=('随机', 'roll'))
 async def rand_num(session: CommandSession) -> None:
     # 取得消息的内容，并且去掉首尾的空白符
     args = session.current_arg_text.strip()
     start = 1
     end = 100
     match_obj: Optional[re.Match] = None
-    if match_obj := re.fullmatch(r"(\d+)-(\d+)", args) is not None:
+    if (match_obj := re.fullmatch(r"(\d+)-(\d+)", args)) is not None:
         start, end = [int(num) for num in match_obj.group(1, 2)]
-    elif match_obj := re.fullmatch(r"(\d+)", args) is not None:
+    elif (match_obj := re.fullmatch(r"(\d+)", args)) is not None:
         end = int(match_obj.group(1))
     elif args != "":
         await session.send(
