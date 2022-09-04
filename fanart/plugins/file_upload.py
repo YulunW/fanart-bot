c55e4ec27705eff295cd0c5c6647d56417cb13e0
Yulun Wang <yulunwan@usc.edu>
2022-09-04 16:48:07 -0700

    Fix file upload

--- fanart/plugins/file_upload.py
+++ fanart/plugins/file_upload.py
@@ -1,11 +1,11 @@
 from nonebot import on_notice, NoticeSession
 from nonebot.log import logger
-from config import MONITORING_GROUPs
+from config import MONITORING_GROUPS
 
 
 @on_notice('group_upload')
 async def upload_recognition(session: NoticeSession):
-    if int(session.ctx.group_id) in MONITORING_GROUPs:
+    if session.ctx.group_id not in MONITORING_GROUPS:
         logger.info(f"Group {session.ctx.group_id} is not in the monitoring groups. Skip this upload")
         return
     await session.send(session.event.__repr__())
