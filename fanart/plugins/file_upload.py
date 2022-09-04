4dad01c9840690b728ab8f1d88855dd912b1778a
Yulun Wang <yulunwan@usc.edu>
2022-09-04 16:46:35 -0700

    Fix file upload

--- fanart/plugins/file_upload.py
+++ fanart/plugins/file_upload.py
@@ -5,7 +5,7 @@ from config import MONITORING_GROUPs
 
 @on_notice('group_upload')
 async def upload_recognition(session: NoticeSession):
-    if session.ctx.group_id in MONITORING_GROUPs:
+    if int(session.ctx.group_id) in MONITORING_GROUPs:
         logger.info(f"Group {session.ctx.group_id} is not in the monitoring groups. Skip this upload")
         return
     await session.send(session.event.__repr__())
